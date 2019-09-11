f17ac18ffc246c09daffcc58fc3f4b075c7ed7ef
weremax <hardcode45@yahoo.com>
2019-09-11 08:12:19 -0400

    remove a removed file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import PostsNew from './components/posts_new';
 import PostsShow from './components/posts_show';
 import Numb from './components/numb';
 import FormTest from './components/formTest';
-import FieldArraysForm from './components/FieldArraysForm';
 import './index.css';
 
 // adding persistance setup
@@ -31,7 +30,6 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/formTest" component={FormTest} />
-          <Route path="/FieldArraysForm" component={FieldArraysForm} />
           <Route path="/numb" component={Numb} />
           <Route path="/posts/new" component={PostsNew} />
           <Route path="/posts/:id" component={PostsShow} />
